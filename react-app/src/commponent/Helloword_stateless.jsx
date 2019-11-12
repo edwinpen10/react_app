78804617cb6e6dae3011e6d7dfccbe768b2c5655
Webslesson <edwinpen010@gmail.com>
2019-11-12 04:04:14 -0500

    stateless and statefull commponent

---
+++ react-app/src/commponent/Helloword_stateless.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Helloword_stateless.css'
+
+function Helloword_stateless(){
+	return <p className="text-p">hallo dunia stateless component</p>
+}
+
+export default Helloword_stateless
---
+++ react-app/src/container/Statefull.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Statefull extends React.Component {
+		render(){
+				return <p>Ini hello dunia pake statefull component</p>
+			}
+	}
+
+
+export default Statefull
